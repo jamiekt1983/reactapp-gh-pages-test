04cceb41844526479a38117240410bdb856279d7
jamiekt1983 <jkthompson@hotmail.co.uk>
2020-05-14 09:59:06 +0100

    Add files via upload

---
+++ build/precache-manifest.dc24f3b3d1fe6640ec37735c8b64699f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a87505b4d44d7bb1895b518f731cd806",
+    "url": "/reactapp-gh-pages-test/index.html"
+  },
+  {
+    "revision": "da36cfcfd3271f9479c5",
+    "url": "/reactapp-gh-pages-test/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "17c79084c1e20441ba34",
+    "url": "/reactapp-gh-pages-test/static/js/2.a489c2db.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/reactapp-gh-pages-test/static/js/2.a489c2db.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "da36cfcfd3271f9479c5",
+    "url": "/reactapp-gh-pages-test/static/js/main.f5d35ec5.chunk.js"
+  },
+  {
+    "revision": "8e7fe1ab90a12f87002a",
+    "url": "/reactapp-gh-pages-test/static/js/runtime-main.ce7fd67d.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/reactapp-gh-pages-test/static/media/logo.5d5d9eef.svg"
+  }
+]);
---
+++ build/service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/reactapp-gh-pages-test/precache-manifest.dc24f3b3d1fe6640ec37735c8b64699f.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/reactapp-gh-pages-test/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+});
